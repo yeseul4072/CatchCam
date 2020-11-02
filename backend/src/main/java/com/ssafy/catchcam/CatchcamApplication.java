fcca9f51050aecdf563d537c338ffa1991058966
cocony12 <apap8510@naver.com>
2020-11-02 14:13:17 +0900

    Add: New backend directory
    Add new backend directory (template)

---
+++ backend/src/main/java/com/ssafy/catchcam/CatchcamApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.catchcam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchcamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchcamApplication.class, args);
+	}
+
+}

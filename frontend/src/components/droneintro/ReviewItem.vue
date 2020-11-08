0611392416a186faf358f4a9a60f7384e8c15adc
yeseul4072 <yeseul4072@gmail.com>
2020-11-08 19:15:27 +0900

    ﻿Feat: 리뷰 페이지 FE
    - reveiwitem 컴포넌트 추가

---
+++ frontend/src/components/droneintro/ReviewItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <hr class="my-hr">
+    <div class="my-3">
+      <div class="d-flex justify-space-between">
+        <div class="d-flex">
+          <div>{{ review.user }}</div>
+          <div class="date-text">{{ review.create_date }}</div>
+        </div>
+        <v-rating
+          :value="review.star_rate"
+          readonly
+          color="yellow darken-3"
+          background-color="grey darken-1"
+          empty-icon="$ratingFull"
+          size="15"
+        ></v-rating>
+      </div>
+      <div>
+        {{ review.content }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReviewItem",
+  props: {
+    review: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style scoped>
+.date-text {
+  color:rgb(148, 148, 148);
+  margin-left: 1rem;
+  font-size: 14px;
+}
+.my-hr {
+  border: 0;
+  height: 1px;
+  background-color:rgb(148, 148, 148);
+}
+
+</style>
--- frontend/src/components/droneintro/Reviews.vue
+++ frontend/src/components/droneintro/Reviews.vue
@@ -1,25 +1,112 @@
 <template>
   <div>
+    <v-card v-if="showReviewForm" outlined color="#23252d" class="mb-5" style="border: 1px solid rgb(148, 148, 148);">
+      <v-container class="d-flex flex-column">
+      <v-textarea
+        color="rgb(148, 148, 148)"
+        counter
+        dark
+        flat
+      ></v-textarea>
+      <div class="d-flex justify-space-between align-center">
+        <div class="d-flex align-center">
+          <div class="reg_text">별점: </div>
+          <v-rating
+            class="align-self-end"
+            v-model="rating"
+            color="yellow darken-3"
+            background-color="grey darken-1"
+            empty-icon="$ratingFull"
+            half-increments
+            hover
+            size="18"
+          ></v-rating>
+        </div>
+        <div class="d-flex align-self-end">
+          <v-btn
+            color="#018F26"
+            outlined
+            dark
+            class="mr-2"
+            @click="reviewForm()"
+          >취소하기</v-btn>
+          <v-btn
+            color="#018F26"
+            dark
+          >저장하기</v-btn>
+        </div>
+        </div>
+      </v-container>
+    </v-card>
+
+    <div class="d-flex justify-space-between align-center">
+      <div class="score_text">
+        {{ reviews[0].avg_rate }} / 5 ({{ reviews[0].count }}개 후기)
+      </div>
+      <div>
+        <v-btn color="#018F26" dark @click="reviewForm()" v-if="!showReviewForm">후기 쓰기</v-btn>
+      </div>
+    </div>
+    <ReviewItem
+    class="mt-3"
+    v-for="review in reviews"
+    :key="review.id"
+    :review="review"
+    />
+
   </div>
 </template>
 
 <script>
+import ReviewItem from '@/components/droneintro/ReviewItem'
+
 export default {
   name: "Reviews",
+  components: {
+    ReviewItem,
+  },
   data() {
     return {
-      review: {
-        content: "아ㅏㅏ니ㅣㅣㅣㅣㅣ",
-        user: "쇼메이커",
-        created_date: "2020. 11. 03. 21:01:09",
-        modify_date: "2020. 11. 03. 21:01:09",
-        star_rate: 3.5,
-      },
-    };
+      showReviewForm: false,
+      reviews: [
+        {
+          avg_rate: 2,
+          count: 2,
+          id: 0,
+          content: '별로에요 쓰지마세요',
+          create_date: '2020-11-08',
+          update_date: '2020-11-08',
+          user: '예슬',
+          star_rate: 1,
+        },
+        {
+          avg_rate: 2,
+          count: 2,
+          id: 1,
+          content: '좋음',
+          create_date: '2020-11-08',
+          update_date: '2020-11-08',
+          user: '예슬이',
+          star_rate: 3,
+        },
+      ]
+    }
   },
-};
+  methods: {
+    reviewForm() {
+      this.showReviewForm = !this.showReviewForm
+    }
+  }
+
+}
 </script>
 
 <style scoped>
-
+.score_text {
+  font-size: 20px;
+  font-weight: bold;
+}
+.reg_text {
+  color: white;
+}
 </style>

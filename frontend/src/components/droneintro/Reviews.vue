9811240963920338ef0168a518d97f565e771a10
yeseul4072 <yeseul4072@gmail.com>
2020-11-08 16:33:24 +0900

    ﻿Feat: 컴포넌트 관계 변경

--- frontend/src/components/droneintro/Reviews.vue
+++ frontend/src/components/droneintro/Reviews.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h1>드론리뷰</h1>
   </div>
 </template>
 
@@ -12,19 +11,15 @@ export default {
       review: {
         content: "아ㅏㅏ니ㅣㅣㅣㅣㅣ",
         user: "쇼메이커",
-        created_at: "2020. 11. 03. 21:01:09",
+        created_date: "2020. 11. 03. 21:01:09",
+        modify_date: "2020. 11. 03. 21:01:09",
+        star_rate: 3.5,
       },
-      // {
-      //     title: '강남점',
-      //     address: '서울특별시 강남구 논현동 138-30',
-      //     phone: '02-3429-5487',
-      //     hourfrom: 9,
-      //     hourto: 17
-      // }
     };
   },
 };
 </script>
 
-<style>
+<style scoped>
+
 </style>

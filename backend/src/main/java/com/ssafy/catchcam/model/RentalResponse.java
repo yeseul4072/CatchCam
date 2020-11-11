75be85474a3f761413809131a74ee3a1a186e6f3
cocony12 <apap8510@naver.com>
2020-11-11 11:12:28 +0900

    Modify: 대여내역 조회
    대여내역 조회 내용 수정
    조회 시 대여상태 추가
    
    S03P31A102-108

--- backend/src/main/java/com/ssafy/catchcam/model/RentalResponse.java
+++ backend/src/main/java/com/ssafy/catchcam/model/RentalResponse.java
@@ -10,7 +10,20 @@ public class RentalResponse {
 	private Date rentDate;
 	private Date returnDate;
 	private Date actReturnDate;
+	private String strRentDate;
+	private String strReturnDate;
+	private String strActReturnDate;
+	private String status;
+	private String itemName;
+	private String profileImg;
+	private int cost;
+	private String storeName;
+	private String telNo;
+	private int openTime;
+	private int closeTime;
+	private String address;
 
+	
 	public RentalResponse() {
 
 	}
@@ -69,6 +82,102 @@ public class RentalResponse {
 
 	public void setActReturnDate(Date actReturnDate) {
 		this.actReturnDate = actReturnDate;
+	}
+
+	public String getStrRentDate() {
+		return strRentDate;
+	}
+
+	public void setStrRentDate(String strRentDate) {
+		this.strRentDate = strRentDate;
+	}
+
+	public String getStrReturnDate() {
+		return strReturnDate;
+	}
+
+	public void setStrReturnDate(String strReturnDate) {
+		this.strReturnDate = strReturnDate;
+	}
+
+	public String getStrActReturnDate() {
+		return strActReturnDate;
+	}
+
+	public void setStrActReturnDate(String strActReturnDate) {
+		this.strActReturnDate = strActReturnDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getProfileImg() {
+		return profileImg;
+	}
+
+	public void setProfileImg(String profileImg) {
+		this.profileImg = profileImg;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public String getStoreName() {
+		return storeName;
+	}
+
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+
+	public String getTelNo() {
+		return telNo;
+	}
+
+	public void setTelNo(String telNo) {
+		this.telNo = telNo;
+	}
+
+	public int getOpenTime() {
+		return openTime;
+	}
+
+	public void setOpenTime(int openTime) {
+		this.openTime = openTime;
+	}
+
+	public int getCloseTime() {
+		return closeTime;
+	}
+
+	public void setCloseTime(int closeTime) {
+		this.closeTime = closeTime;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
 	}
 	
 	

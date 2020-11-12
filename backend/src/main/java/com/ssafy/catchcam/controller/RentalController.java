256e37c4e51f33c8329a4f0561d7ef3745397819
YongjoonSeo <koreakkrea12@naver.com>
2020-11-12 09:51:15 +0900

    Chore: RequestMapping /api 추가
    - 백엔드 서버 컨트롤러에 RequestMapping /api 추가

--- backend/src/main/java/com/ssafy/catchcam/controller/RentalController.java
+++ backend/src/main/java/com/ssafy/catchcam/controller/RentalController.java
@@ -6,13 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ssafy.catchcam.model.CommonResponse;
 import com.ssafy.catchcam.model.RentalRequest;
@@ -24,6 +18,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
+@RequestMapping("/api")
 @RestController
 public class RentalController {
 
--- backend/src/main/java/com/ssafy/catchcam/controller/UserController.java
+++ backend/src/main/java/com/ssafy/catchcam/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.catchcam.model.CommonResponse;
@@ -16,6 +17,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
+@RequestMapping("/api")
 @RestController
 public class UserController {
 

d39ddeff766c29c6435c925d8f3a4dd613b644f4
YongjoonSeo <koreakkrea12@naver.com>
2020-11-17 20:37:28 +0900

    Chore: API 요청주소 Dev 서버로 변경
    - api.js 루트 요청주소 dev서버로 변경

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -8,7 +8,7 @@ export default new Vue({
     },
     axios() {
       return axios.create({
-        baseURL: "https://catchcam.site/api",
+        baseURL: "http://k3a102.p.ssafy.io:8000/api",
         headers: {
           "x-auth-token": this.token ? this.token : '',
           "Content-type": "application/json"
